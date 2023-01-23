cc4bc08f29171c731a61dfcebdb3b1002390e87e
Anastasiya <anastasiya.sachuk@mail.ru>
2023-01-23 20:42:37 +0300

    Add task 2

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -4,3 +4,22 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125*/
 
+/* Напишите праграмму, которая принимает на вход число(N) и выдает таблицу квадратов 
+чисел от 1 до N. Пример:
+5 -> 1, 4, 9, 16, 25
+2 -> 1, 4 */
+
+Console.Clear();
+Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;
+// Вариант 1
+//Console.Write($"{Math.Pow(count, 2)}");
+//count++;
+
+while (count <= number)
+{
+    Console.Write($"{Math.Pow(count, 3)}");
+    if (count != number) Console.Write(", "); //Второй вариант того что бы после последней цифра небыло запятой
+    count++;
+}
